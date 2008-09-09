f458ca74eebf0d6b7c558156130af3f8862df212
PJ Kelly <pj@boomdesigngroup.com>
2008-09-09 00:10:31 -0700

    Overriding tasks in the deploy namespace to use passenger-specific tasks when :app_server is set to passenger.

--- lib/railsmachine/recipes/app/passenger.rb
+++ lib/railsmachine/recipes/app/passenger.rb
@@ -6,24 +6,45 @@ Capistrano::Configuration.instance(:must_exist).load do
  
   namespace :passenger do
 
-    namespace :deploy do
-
-      [:start, :stop].each do |t|
-        desc "The :#{t} task no effect when using Passenger as your application server."
-        task t, :roles => :app do
-          puts "The :#{t} task no effect when using Passenger as your application server."
-        end
+    [:start, :stop].each do |t|
+      desc "The :#{t} task no effect when using Passenger as your application server."
+      task t, :roles => :app do
+        puts "The :#{t} task no effect when using Passenger as your application server."
       end
+    end
 
-      desc <<-DESC
-      Restart the Passenger processes on the app server by touching tmp/restart.txt.
-      DESC
-      task :restart, :roles => :app do
-        run "touch #{current_path}/tmp/restart.txt"
-      end
-  
+    desc <<-DESC
+    Restart the Passenger processes on the app server by touching tmp/restart.txt.
+    DESC
+    task :restart, :roles => :app do
+      run "touch #{current_path}/tmp/restart.txt"
     end
 
   end
 
+  namespace :deploy do
+    
+    desc <<-DESC
+    This has no effect when using passenger.
+    DESC
+    task :start, :roles => :app do
+      passenger.start
+    end
+  
+    desc <<-DESC
+    Restart passenger.
+    DESC
+    task :restart, :roles => :app do
+      passenger.restart
+    end
+  
+    desc <<-DESC
+    This has no effect when using passenger.
+    DESC
+    task :stop, :roles => :app do
+      passenger.stop
+    end
+  
+  end
+  
 end
